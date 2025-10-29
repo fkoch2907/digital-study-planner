dd40b272822b6b76d0fa906e392a6cc14fdbcc63
bohong <bohongli2001@gmail.com>
2025-10-29 16:25:53 +0100

    created a base calendar object structure and methods such as checking the calendar's existence, loading an existing calendar or creating a new one

---
+++ src/digital-study-planner/base.py
@@ -0,0 +1,57 @@
+"""
+This file stores the definition of the calendar class and its methodes.
+
+We want to create a calendar-like object (final product) in this project.
+
+This calendar should contain the following methodes (functionalities):
+
+- It automatically shows up the current date/time and the events on the day
+- It can create a new, empty calendar
+- It can add new events with an event name, start/end time, repeat period, and its event type
+- It can view events that occur in a month/week/day
+- It includes a to-do list. A list of tasks with/without any specific time settings, and can be viewed and edited at
+any time
+    - (for the end) with overlay decoration
+    - Can also automatically include events that occur within a time range (user can set it), such as including events
+    within 3 days
+- task reminder/notification system for hand-in, deadlines
+    - with a day counter
+
+first version: interactive via the terminal window
+"""
+
+import os
+
+import pandas as pd
+import pickle
+
+class DigitalCalendar:
+    """Start a new calendar or access an already existing calendar."""
+
+    def __init__(self, filename):
+        """Initialize the calendar."""
+        self.calendar_name = f"{filename}.pckl"
+        self.calendar = self.check_exists()
+        self.events = {}
+
+    def check_exists(self):
+        if os.path.exists(self.calendar_name):
+            print(f"You are checking out your calendar: {self.calendar_name}")
+            return self.load_calendar()
+
+        else:
+            print(f"You are creating a new calendar: {self.calendar_name}")
+            return self.create_calendar()
+
+    def load_calendar(self):
+        with open(self.calendar_name, "rb") as f:\
+            return pickle.load(f)
+
+    def create_calendar(self):
+        new_calendar = pd.DataFrame({
+            self.calendar_name: [],
+        })
+        with open(self.calendar_name, "wb") as f:
+            pickle.dump(new_calendar, f)
+
+        return new_calendar
